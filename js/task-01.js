9f3844e3a1ab5b9185903a01dd69fd5f3529706a
Roman-GoIT <best4you.ua@gmail.com>
2022-12-29 17:40:42 +0200

    update

--- js/task-01.js
+++ js/task-01.js
@@ -1,7 +1,18 @@
 //кількість категорій
-const categoriesNumber = document.querySelectorAll('li.item');
-console.log('Number of categories:', categoriesNumber);
+//const categoriesNumber = document.querySelectorAll('li.item');
+//console.log('Number of categories:', categoriesNumber);
 
 //кількість елементів
-const elementsNumber = document.querySelectorAll('.option');
-console.log(elementsNumber);
+//const elementsNumber = document.querySelectorAll('.option');
+//console.log(elementsNumber);
+
+
+//console.log(categoriesNumber)
+function quantity(categoriesNumber) {
+    let totalNumber = 0;
+
+    categoriesNumber.forEach((item) => totalNumber += item);
+      return totalNumber;
+      console.log(totalNumber);
+}
+
