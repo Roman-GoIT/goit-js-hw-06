110710c0d11117a83017aaeef625cd8b5ce80181
Roman-GoIT <best4you.ua@gmail.com>
2023-01-11 12:02:19 +0200

    task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const recepy = document.querySelector('#ingredients');
+
+const categories = ingredients.map(ingredient => {
+ 
+  const row = document.createElement('li');
+  row.textContent = ingredient;
+  row.classList.add('item');
+
+  return row;
+})
+
+recepy.append(...categories);
+console.log(categories);
+console.log(recepy.textContent);
