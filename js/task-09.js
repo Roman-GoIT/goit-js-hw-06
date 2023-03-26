dc4a18550c640d8fe54844308476786d785d253d
Roman-GoIT <best4you.ua@gmail.com>
2023-03-27 01:30:09 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+
+const body = document.querySelector('body');
+const button = document.querySelector('.change-color');
+const  color = document.querySelector('.color');
+
+button.addEventListener('click', changeColor);
+
+function changeColor() {
+  color.textContent = getRandomHexColor();
+
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+   
+  body.style.backgroundColor = color.textContent;
+};
